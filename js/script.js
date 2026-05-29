95a052f84cb0bfdc6bf0a4fe76f069ff44e8bf97
Diegoalexander717 <20250033@ricaldone.edu.sv>
2026-05-29 12:14:31 -0600

    ejercicios

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let num1 = pasrseFloa(prompt("Ingreese el valor 1"))
+let num2 = pasrseFloa(prompt("Ingreese el valor 2"))
+let num3 = pasrseFloa(prompt("Ingreese el valor 3"))
+const promedio = Math.sqrt (num1 + num2 + num3)
